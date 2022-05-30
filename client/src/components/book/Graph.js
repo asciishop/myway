2ba7d1a309b39d17ce1377fc9e58855a28726944
asciishop <andres.vasquez.perez@gmail.com>
2022-05-30 07:56:16 -0400

    read chapter cap 1

--- client/src/components/book/Graph.js
+++ client/src/components/book/Graph.js
@@ -6,56 +6,57 @@ import AudioNode from "./AudioNode";
 import VideoNode from "./VideoNode";
 import Edge from "./Edge";
 import {Link, useParams} from "react-router-dom";
-
-
+import {fetchBook} from "../../actions";
+import {connect} from "react-redux";
+import map from 'lodash/map';
 
 
 class Graph extends Component {
 
-    componentDidMount() {
-        const { bookid } = this.props.match.params;
-
-
-    }
 
     render() {
 
+        const { bookid } = this.props.match.params;
+        this.props.fetchBook(bookid,this.props.history);
+        console.log(this.props.book)
+
         return (
-<div>
-            <div className="fixed-action-btn">
-                <Link to="/chapter/new" className="btn-floating btn-large red">
-                    <i className="material-icons">add</i>
-                </Link>
+            <div>
+                <div className="fixed-action-btn">
+                    <Link to="/chapter/new" className="btn-floating btn-large red">
+                        <i className="material-icons">add</i>
+                    </Link>
+                </div>
+
+                <div className='content'>
+                    {this.props.book.chapters.map(function(node, index){
+                        switch(node.type) {
+                            case "image":
+                                return <div><ImageNode node={node} /><Edge /></div>
+                                break;
+                            case "text":
+                                return <div><TextNode node={node} /><Edge /></div>
+                                break;
+                            case "video":
+                                return <div><VideoNode node={node} /><Edge /></div>
+                                break;
+                            case "audio":
+                                return <div><AudioNode node={node} /><Edge /></div>
+                                break;
+                        }
+
+
+                    })}
+                </div>
             </div>
-
-                                <div className='content'>
-                                    {chapters.map(function(node, index){
-                                        switch(node.type) {
-                                            case "image":
-                                                return <div><ImageNode node={node} /><Edge /></div>
-                                                break;
-                                            case "text":
-                                                return <div><TextNode node={node} /><Edge /></div>
-                                                break;
-                                            case "video":
-                                                return <div><VideoNode node={node} /><Edge /></div>
-                                                break;
-                                            case "audio":
-                                                return <div><AudioNode node={node} /><Edge /></div>
-                                                break;
-                                        }
+        )
 
 
-                                    })}
-
-                                </div>
-</div>
-
-        );
     }
 }
 
+function mapStateToProps({ book }) {
+    return { book};
+}
 
-export default Graph;
-
-
+export default connect(mapStateToProps, { fetchBook})(Graph);
