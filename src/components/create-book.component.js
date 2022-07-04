1d39a5fce80b67cc2d1ded9a7224f749abe67ddf
amperezio <amperez@stefanini.com>
2022-07-04 07:11:47 -0400

    audio and video added

--- src/components/create-book.component.js
+++ src/components/create-book.component.js
@@ -4,20 +4,24 @@ import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import {URL_BACKEND} from "./const";
 import {Card,Tab,Row,Col,Nav} from "react-bootstrap";
-import VideoNode from "./VideoNode";
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+import MicRecorder from 'mic-recorder-to-mp3';
 
 
-
+const Mp3Recorder = new MicRecorder({ bitRate: 128 });
 
 export default class Createbook extends Component {
 
   constructor(props) {
     super(props)
 
+
     // Setting up functions
     this.handleOnChangeTitle = this.handleOnChangeTitle.bind(this);
     this.handleOnChangeContent = this.handleOnChangeContent.bind(this);
     this.handleInputFileChange = this.handleInputFileChange.bind(this);
+    this.handleTakePhoto = this.handleTakePhoto.bind(this);
     this.handleClick = this.handleClick.bind(this);
 
 
@@ -27,9 +31,19 @@ export default class Createbook extends Component {
       title : '',
       content : '',
       file : '',
+      isRecording: false,
+      blobURL: '',
+      isBlocked: false,
     }
   }
 
+  handleTakePhoto (dataUri) {
+    // Do stuff with the photo...
+    console.log('takePhoto');
+    console.log(dataUri);
+    this.setState({ file: dataUri })
+
+  }
 
   handleClick() {
 
@@ -72,13 +86,59 @@ export default class Createbook extends Component {
 
     }
   }
+  componentDidMount() {
+    navigator.getUserMedia({ audio: true },
+        () => {
+          console.log('Permission Granted');
+          this.setState({ isBlocked: false });
+        },
+        () => {
+          console.log('Permission Denied');
+          this.setState({ isBlocked: true })
+        },
+    );
+  }
 
+  start = () => {
+    if (this.state.isBlocked) {
+      console.log('Permission Denied');
+    } else {
+      Mp3Recorder
+          .start()
+          .then(() => {
+            this.setState({ isRecording: true });
+          }).catch((e) => console.error(e));
+    }
+  };
 
+  stop = () => {
+    Mp3Recorder
+        .stop()
+        .getMp3()
+        .then(async ([buffer, blob]) => {
+          const blobURL = URL.createObjectURL(blob)
+          this.setState({blobURL, isRecording: false});
+          const audioBase64 = await this.blobToBase64(blob);
+          this.setState({file: audioBase64})
+
+        }).catch((e) => console.log(e));
+  };
+
+  blobToBase64 = (blob) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    return new Promise((resolve) => {
+      reader.onloadend = () => {
+        resolve(reader.result);
+      };
+    });
+  };
   render() {
     return (
 
         <div className="container-fluid">
           <div className="row d-flex justify-content-center"><div className="col d-flex align-items-center justify-content-center pt-3"> <h2>Ingrese un libro</h2>          </div></div>
+
           <br/>
           <div className="form-group">
             <label htmlFor="title">Titulo</label>
@@ -101,7 +161,13 @@ export default class Createbook extends Component {
                       <Nav.Link eventKey="first">Texto</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">Imagen, Audio o Video</Nav.Link>
+                      <Nav.Link eventKey="second">Imagen</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Audio</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="fourth">Archivo</Nav.Link>
                     </Nav.Item>
                   </Nav>
                 </Col>
@@ -118,6 +184,19 @@ export default class Createbook extends Component {
                       />
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
+
+                      <Camera
+                          onTakePhoto = { (dataUri) => { this.handleTakePhoto(dataUri); } }
+                      />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+
+                      <button onClick={this.start} disabled={this.state.isRecording}>Record</button>
+                      <button onClick={this.stop} disabled={!this.state.isRecording}>Stop</button>
+                      <audio src={this.state.blobURL} controls="controls" />
+
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="fourth">
 
                       <input type="file"
                              className="form-control"
